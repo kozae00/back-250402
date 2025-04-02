e6977f04320e8b56947ebd45702b8c551da77bcb
kozae00 <rhwogus777@gmail.com>
2025-04-02 09:47:36 +0900

    feat: controller

--- src/main/kotlin/com/example/back/BackApplication.kt
+++ src/main/kotlin/com/example/back/BackApplication.kt
@@ -1,11 +1,11 @@
-package com.example.demo
+package com.example.back
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class DemoApplication
+class BackApplication
 
 fun main(args: Array<String>) {
-	runApplication<DemoApplication>(*args)
+	runApplication<BackApplication>(*args)
 }
